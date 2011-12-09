0959df35adc213b41eb8ce95c3e8b6793215840e
Elliot Woods <elliot.woods@gmail.com>
2011-12-09 12:21:34 +0100

    some tidying

--- src/ofxTalky.h
+++ src/ofxTalky.h
@@ -9,7 +9,6 @@
 
 #include "TalkyBase.h"
 
-
 #include "ofxNetwork.h"
 #include "ofMain.h"
 
@@ -23,63 +22,63 @@ class ofxTalky : public ofThread, public TalkyBase
 {
 public:
 	~ofxTalky();
-    
+
     ///setup as client
 	void init(string remoteHost, int remotePort=TALKY_DEFAULT_PORT);
-	
+
 	///setup as server
 	void init(int localPort=TALKY_DEFAULT_PORT);
-	
+
 	///We keep the old syntax for backwards compatability
 	void setup(int localPort=TALKY_DEFAULT_PORT) { init(localPort); };
 	void setup(string remoteHost, int remotePort=TALKY_DEFAULT_PORT) {init(remoteHost, remotePort); };
-	
+
 	bool	getIsServerBound();
 	int		getNumClients();
-    
+
     Poco::BasicEvent<int>		actionMsgRx;
 	Poco::BasicEvent<int>		actionClientIsNowConnected;
-	
+
 protected:
     void threadedFunction();
-    
-    void beginThread(); 
-    
+
+    void beginThread();
+
     bool lockThread();
     void unlockThread();
     bool lockServer();
     void unlockServer();
-    
+
     void initClient();
     void initServer();
-    
+
     void startClient();
     void startServer();
-    
+
     bool isClientConnected();
     bool isServerConnected();
     bool isServersClientConnected(int iClient);
-	
+
     int getNumServerClients();
-	
+
     int rxServer(int iClient, char *buffer, int bufferSize);
     int rxClient(char *buffer, int bufferSize);
-	
+
     void txServer(int iClient, char *buffer, int messageSize);
     void txClient(char *buffer, int messageSize);
 
     void notifyReceiveEvent(int msgCount);
 	void notifyClientIsNowConnected();
-	
+
 	void throwWarning(string s);
 	void throwError(string s);
-	
+
 	/** In openFrameworks' ofxNetwork,
 	 network errors arrive as negative
 	 byte counts. We process them here.
 	 */
 	void processRxErrors(int& n);
-    
+
 	ofxTCPServer	*tcpServer;
 	ofxTCPClient	*tcpClient;
 };
